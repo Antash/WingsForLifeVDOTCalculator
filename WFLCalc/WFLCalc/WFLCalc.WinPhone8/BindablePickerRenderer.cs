f16bf61da208726f39e021b8f2cb9e33a6fb1907
Anton Ashmarin <aashmarin@gmail.com>
2017-03-28 08:52:40 +0300

    bindable picker issues wip

--- WFLCalc/WFLCalc/WFLCalc.WinPhone8/BindablePickerRenderer.cs
+++ WFLCalc/WFLCalc/WFLCalc.WinPhone8/BindablePickerRenderer.cs
@@ -9,7 +9,7 @@ using WFLCalc.WinPhone8;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.WinPhone;
 
-[assembly: ExportRenderer(typeof(BindablePicker), typeof(BindablePickerRenderer))]
+//[assembly: ExportRenderer(typeof(BindablePicker), typeof(BindablePickerRenderer))]
 
 namespace WFLCalc.WinPhone8
 {
@@ -20,13 +20,19 @@ namespace WFLCalc.WinPhone8
         protected override void OnElementChanged(ElementChangedEventArgs<BindablePicker> e)
         {
             base.OnElementChanged(e);
-            this.picker = e.NewElement;
-            var listPicker = new ListPicker();
-            listPicker.ItemsSource = new[] { 1, 2, 4, 7, 6 };//picker.ItemsSource;
+            //this.picker = e.NewElement;
+            //var listPicker = new ListPicker();
+            //listPicker.ItemsSource = picker.ItemsSource;
             //listPicker.ItemTemplate = 
-            listPicker.SelectedItem = 1;// picker.SelectedItem;
+            //listPicker.SelectedItem = picker.SelectedItem;
+            //listPicker.SelectionChanged += OnSelectionChanged;
             //listPicker.FullModeItemTemplate;
-            this.SetNativeControl(listPicker);
+            //this.SetNativeControl(listPicker);
+        }
+
+        private void OnSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            //picker.SelectedIndex = e.AddedItems[0];
         }
     }
 }
